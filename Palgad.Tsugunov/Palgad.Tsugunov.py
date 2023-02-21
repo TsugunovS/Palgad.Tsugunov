c5437258559d606e5b9bfd4f2c13756cb49bf55c
TsugunovS <stanislav.tsugunov@gmail.com>
2023-02-21 14:12:35 +0200

    Add project files.

---
+++ Palgad.Tsugunov/Palgad.Tsugunov.py
@@ -0,0 +1,21 @@
+﻿from math import * 
+from random import *
+from module1 import * 
+#21/02/23
+#Практическая работа "Palgad"
+#9. Top() - Т самых бедных и самых богатых человека
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+
+while True:
+    nimi = input("Sisestage nimi (väljumiseks sisestage 'väljund'): ")
+    if nimi == 'väljund':
+        break
+    inimesed.append(nimi)
+    palg = int(input("Sisestage palgad: "))
+    palgad.append(palg)
+
+
+
+
+
