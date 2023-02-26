8425056510997552ae138ed77b19e54a19b11901
TsugunovS <120027765+TsugunovS@users.noreply.github.com>
2023-02-27 01:40:29 +0200

    Top

--- Palgad.Tsugunov/Palgad.Tsugunov.py
+++ Palgad.Tsugunov/Palgad.Tsugunov.py
@@ -4,16 +4,45 @@ from module1 import *
 #21/02/23
 #Практическая работа "Palgad"
 #9. Top() - Т самых бедных и самых богатых человека
+
+#3
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+#leiab palgad nimekirjast maksimumpalga
+#находит максимальную зарплату в списке palgad
+max_palga_indeks = palgad.index(max(palgad))
+max_palga_saanu = inimesed[max_palga_indeks]
+#näitab maksimumpalka
+#показывает максимальную зарплату
+print(f" Kõrgeim palk ({max(palgad)}) saab {max_palga_saanu}.")
+
+
+
+#9
 palgad = [1200, 2500, 750, 395, 1200]
 inimesed = ["A", "B", "C", "D", "E"]
 
-while True:
-    nimi = input("Sisestage nimi (väljumiseks sisestage 'väljund'): ")
-    if nimi == 'väljund':
-        break
-    inimesed.append(nimi)
-    palg = int(input("Sisestage palgad: "))
-    palgad.append(palg)
+# liita loendid loendiks
+# объединить списки в список
+andmed = list(zip(palgad, inimesed))
+
+# sorteerige loend kasvavas järjekorras
+# сортируем список по возрастанию зарплаты
+andmed.sort()
+
+# väljund T vaesemad inimesed
+# выводим T самых бедных человек
+T = 2
+print(f"T Kõige vaesemad inimesed: {andmed[:T]}")
+
+# sorteerige loend kahanevas järjekorras
+# сортируем список по убыванию
+andmed.sort(reverse=True)
+
+# väljund T rikkaimad inimesed
+# выводим T самых богатых человек
+print(f"T Kõige rikkaim inimesed: {andmed[:T]}")
+
 
 
 
--- Palgad.Tsugunov/module1.py
+++ Palgad.Tsugunov/module1.py
@@ -1,8 +1,15 @@
-﻿palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
-def 
-    
-
+﻿def Top(palgad, inimesed, T):
+    n = len(palgad)
+    if T >= n:
+        return inimesed
+    else:
+        # Sorteerimine
+        # Сортируем
+        sort_index = sorted(range(n), key=lambda i: palgad[i])
+        # Saame T vaeseima ja T rikkaima nimekirja
+        # Получаем список из T самых бедных и T самых богатых
+        top = inimesed[sort_index[:T]] + inimesed[sort_index[-T:]]
+        return top
 
     
 
